6cb4cd1589c1b9d7332942ddc7616888e32c4555
Joe Flanagan <josephptflanagan@gmail.com>
2020-10-07 11:29:08 -0500

    add thought, reaction, friend all functional on back end

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,16 +4,19 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  signToken: function({ username, email, _id }) {
+
+  signToken: function ({ username, email, _id }) {
+
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  
   },
 
-  authMiddleware: function({ req }) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
-  
+
     // separate "Bearer" from "<tokenvalue>"
     if (req.headers.authorization) {
       token = token
@@ -21,12 +24,12 @@ module.exports = {
         .pop()
         .trim();
     }
-  
+
     // if no token, return request object as is
     if (!token) {
       return req;
     }
-  
+
     try {
       // decode and attach user data to request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
@@ -34,7 +37,7 @@ module.exports = {
     } catch {
       console.log('Invalid token');
     }
-  
+
     // return updated request object
     return req;
   }
